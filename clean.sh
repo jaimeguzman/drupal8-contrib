de8bfa90ff3e534244d296d9a4e1bdc02ba6db12
me <me@jguzman.cl>
2015-06-13 13:22:52 -0400

    little script to work on my snadbox

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 
+echo "deleting folders and file olders \n"
+
+rm -fr core example.gitignore modules profiles web.config README.txt autoload.php composer.json index.php robots.txt themes   &
+
+echo "folder cleaning"
